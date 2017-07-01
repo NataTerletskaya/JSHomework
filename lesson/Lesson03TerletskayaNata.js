9eda87525ecb04c839f4a29f9333f3f5b1443331
Nata Terletskaya <nataterle@gmail.com>
2017-07-02 01:04:39 +0300

    Updates in Lesson 3

--- lesson/Lesson03TerletskayaNata.js
+++ lesson/Lesson03TerletskayaNata.js
@@ -7,7 +7,7 @@ console.log("Initial array:\n", arr, "\n");
 // (скажем, длиной в 10 элементов) по возрастанию индекса массива (т.е. слева направо)
 console.log("Используя ТОЛЬКО цикл for, написать скрипт, который выведет значения произвольного массива \n(скажем, длиной в 10 элементов) по возрастанию индекса массива (т.е. слева направо)");
 
-for (i = 0; i < arr.length; i++) {
+for (var i = 0; i < arr.length; i++) {
    console.log(arr[i]);
 }
 
@@ -15,7 +15,7 @@ for (i = 0; i < arr.length; i++) {
 console.log("\nчасть вторая: и наоборот (справа налево)");
 
 var arr = ["Na", "Ta", "Li", "Ya", "Pavl", "Ter", "Le", "Ts", "Ka", "Ya"];
-for (i = arr.length-1; i >= 0; i--){
+for (var i = arr.length-1; i >= 0; i--){
    console.log(arr[i]);
 }
 
@@ -34,7 +34,7 @@ console.log("Array with Random nubers" + "\n" + ranArr)
 // 4.Написать скрипт, который будет искать в произвольном массиве какое-то число, 
 //например число 7. Если число будет найдено, скрипт должен вывести индекс этого числа.
 var numberArr = [152, 56, 78, 7, 12, 7, 96, 85, 12];
-for (i = 0; i < numberArr.length; i++) {
+for (var i = 0; i < numberArr.length; i++) {
     if (numberArr[i] == 7) {
         console.log("Index of 7 is: " + i);
     };
@@ -42,7 +42,7 @@ for (i = 0; i < numberArr.length; i++) {
 
 // 5.Написать скрипт, который найдет все элементы в массиве, которые делятся на 2 и выведет их индексы.
 var numberArr = [152, 56, 78, 7, 12, 7, 96, 85, 12];
-for (i = 0; i < numberArr.length; i++) {
+for (var i = 0; i < numberArr.length; i++) {
     if (numberArr[i] % 2 == 0) {
         console.log("Index of Even Number: " + i);
     }
